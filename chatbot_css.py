fb62d2942e0a4efea5adb55023400f2a9b246449
afry23 <9861676+afry23@users.noreply.github.com>
2023-11-18 07:43:16 +0100

    Change css.

--- chatbot_css.py
+++ chatbot_css.py
@@ -1,41 +1,51 @@
 # CSS-Definition
 css = """
-/* Grundlegende Styling-Einstellungen */
 body {
     font-family: Arial, sans-serif;
+    font-size: 14px;
+    color: black;
+    line-height: 1.5;
     margin: 0;
     padding: 10px;
-    background-color: #f0f0f0;
+    background-color: #827e7e;
 }
 
 /* Stil für Chat-Nachrichten */
 .message {
-    max-width: 60%;
+    max-width: 90%;
     margin-bottom: 10px;
     padding: 8px;
     border-radius: 15px;
-    color: white;
+    color: black;
 }
 
 /* Stil für Benutzer-Nachrichten */
 .user {
-    background-color: #007bff;
+    background-color: #dcf8c6;
     align-self: flex-start;
     border-bottom-left-radius: 0;
 }
 
 /* Stil für Bot-Nachrichten */
 .bot {
-    background-color: #28a745;
+    background-color: #ffffff;
     align-self: flex-end;
     border-bottom-right-radius: 0;
     text-align: right;
+}
+.avatar {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    display: inline-block;
+    margin-right: 10px;
 }
 
 /* Flex-Container für Nachrichten */
 .container {
     display: flex;
     flex-direction: column;
+    background-color: #f7f7f7;
 }
 """
 
